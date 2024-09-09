fe77e2cc5ede9dae0ff20ba223fe1e6ca4ec5dc9
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-09-09 17:42:01 -0300

    modulo2

--- Modulo2/ex6.js
+++ Modulo2/ex6.js
@@ -1,1 +1,14 @@
 // reduce
+
+let notas = [10, 5, 15, 20]
+
+let valorInicial = 0;
+
+let soma = notas.reduce(function(total, atual) {
+    console.log(`Total é ${total}`)
+    console.log(`Atual é ${atual}`)
+    console.log("---")
+    return total + atual
+}, valorInicial);
+
+console.log(`A soma é ${soma}`)
