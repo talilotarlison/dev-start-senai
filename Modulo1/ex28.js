1ea0866eafc00fc0a09c49a874b1aed85f6cf1b3
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-09-08 22:23:42 -0300

    modulo1

---
+++ Modulo1/ex28.js
@@ -0,0 +1,27 @@
+function obterDescricaoDaIdade(idade) {
+    return "Você tem atualmente " + idade + " anos" 
+ }
+ 
+ // Não modifique as linhas abaixo
+ console.log(obterDescricaoDaIdade(18))
+ console.log(obterDescricaoDaIdade(20))
+
+
+ 
+ function mostrarConfirmacaoDeEmail(email) {
+    return "Acabamos de enviar um e-mail para " +  email + " . Por favor, verifique sua caixa de entrada."
+  }
+  
+  // Não modifique as linhas abaixo
+  console.log(mostrarConfirmacaoDeEmail("ana@siteexemplo.com"))
+  console.log(mostrarConfirmacaoDeEmail("alex@siteexemplo.com"))
+
+
+
+  function obterNomeCompleto(nome, sobrenome) {
+    return nome + " " + sobrenome
+}
+
+// Não modifique as linhas abaixo
+console.log(obterNomeCompleto("Alex", "Seixas")) // "Alex Seixas"
+console.log(obterNomeCompleto("Cássia", "Moreira")) // "Cássia Moreira"
---
+++ Modulo1/ex29.js
@@ -0,0 +1,19 @@
+// Definindo a matriz notas
+let notas = [18, 13, 20, 15];
+
+// Exibindo a matriz no console
+console.log(notas);
+
+
+// Definindo a matriz temperaturas
+let temperaturas = [3, 0, -4, 2, 9, 12, 10];
+
+// Exibindo a matriz no console
+console.log(temperaturas);
+
+
+// Definindo a matriz dias
+let dias = [1, 2, 3, 4, 5, 6, 7];
+
+// Exibindo a matriz no console
+console.log(dias);
