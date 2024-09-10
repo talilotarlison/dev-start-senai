402e370e4bbc17e07b6bbceea38c5d7642f34391
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-09-09 23:46:22 -0300

    modulo2

--- Modulo2/ex13.js
+++ Modulo2/ex13.js
@@ -1,3 +1,8 @@
+//Sua matrícula no TOTVS Educacional foi realizada com sucesso.
+//Registro Acadêmico: 00588802
+
+//https://sgeweb.senai.br/FrameHTML/web/app/Edu/PortalProcessoSeletivo/?c=3&ps=187&ai=1701&turma=APE.323.T1#/es/informacoes
+
 function adicionarItem(itens, item) {
     itens.push(item)
     return itens
