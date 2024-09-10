feb9e8331bd816a41947abbf25424aa5c9b2a2e5
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-09-10 12:13:32 -0300

    module3

---
+++ ex13.js
@@ -0,0 +1,11 @@
+class Usuario {
+    // classe vazia
+    constructor(usuario){
+        this.usuario = usuario
+    }
+    
+}
+
+// instancie o objeto aqui
+let caio = new Usuario("kaio")
+console.log(caio)
