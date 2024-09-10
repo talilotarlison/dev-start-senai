cade0584dd7d34dc3e7583e526075b5ec4fe8145
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-09-10 11:01:23 -0300

    modulo3

---
+++ Modulo3/ex1.js
@@ -0,0 +1,20 @@
+function obterAno(estudante) {
+    return estudante.curso.ano
+}
+
+function obterNota(estudante) {
+    return estudante.curso.nota 
+}
+
+// Não modifique as linhas abaixo
+let estudante = {
+    nome: "Andres",
+    idade: 24,
+    curso: {
+        ano: 2020,
+        nota: 8
+    }
+};
+
+console.log(obterAno(estudante)); // 2020
+console.log(obterNota(estudante)); // 8
