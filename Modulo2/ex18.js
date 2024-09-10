7b601e0d82bbc5b723b403faab5892424c83581e
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-09-10 02:01:27 -0300

    modulo2

---
+++ Modulo2/ex18.js
@@ -0,0 +1,36 @@
+let trabalho = {
+    profissao : "Dentista",
+    ano: 2020,
+    empresa: "Valicon"
+  }
+  
+  
+  let socia = {
+      primeiroNome: "Sônia",
+      sobrenome: "D'ávila",
+      trabalho :trabalho
+  };
+  console.log(socia)
+
+
+
+  function objetoNoLog(obj) {
+    console.log(obj)
+}
+
+// Não modifique as linhas abaixo
+let pessoa = {
+    nome: "Samuel",
+    sobrenome: "Duarte",
+    idade: 21,
+    email: "samuelduarte@email.com",
+    estaVerificado: true
+};
+
+let config = {
+    tema: "escuro",
+    moeda: "real"
+};
+
+objetoNoLog(pessoa);
+objetoNoLog(config);
---
+++ Modulo2/ez19.js
@@ -0,0 +1,8 @@
+// Criar objeto restaurante
+let restaurante = {
+    nome : "VWM ",
+    classificacao : 5,
+    abertoNasSegundas : false
+  }
+  
+  console.log(restaurante)
